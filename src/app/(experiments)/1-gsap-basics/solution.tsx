21388a5a58fb6e9970e35517325e2c8e3fb24247
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2025-11-16 19:21:22 -0300

    update experiment

--- src/app/(experiments)/1-gsap-basics/solution.tsx
+++ src/app/(experiments)/1-gsap-basics/solution.tsx
@@ -28,15 +28,17 @@ export default function Page() {
   );
 
   return (
-    <div
-      ref={containerRef}
-      className="bg-blue-300 text-black flex h-screen items-end justify-left overflow-hidden"
-    >
-      <h1 className="title font-black text-[20rem] leading-none pb-[0.1em] text-left">
-        GSAP
-        <br />
-        tweens
-      </h1>
+    <div className="bg-blue-300 text-black">
+      <div
+        ref={containerRef}
+        className="bg-blue-300 text-black flex h-screen items-end justify-left overflow-hidden"
+      >
+        <h1 className="title font-black text-[20rem] leading-none pb-[0.1em] text-left">
+          GSAP
+          <br />
+          tweens
+        </h1>
+      </div>
     </div>
   );
 }
